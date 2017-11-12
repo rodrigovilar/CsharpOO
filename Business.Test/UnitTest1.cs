9facb6db8a90f24b927f4aee08968dc68e352613
Rodrigo Vilar <rvilar@gmail.com>
2017-11-12 09:54:40 -0300

    Teste de propriedades

--- Business.Test/UnitTest1.cs
+++ Business.Test/UnitTest1.cs
@@ -18,5 +18,19 @@ namespace Business.Test
             Assert.NotNull(_contaBancaria);
         }
 
+        [Fact]
+        public void Properties() 
+        {
+            _contaBancaria.banco = "BB";
+            _contaBancaria.agencia = 1000;
+            _contaBancaria.numero = 123456;
+            _contaBancaria.tipo = TipoConta.Poupanca;
+
+            Assert.Equal(_contaBancaria.banco, "BB");
+            Assert.Equal(_contaBancaria.agencia, 1000);
+            Assert.Equal(_contaBancaria.numero, 123456);
+            Assert.Equal(_contaBancaria.tipo, TipoConta.Poupanca);
+        }
+    
     }
 }
--- Business/Business.cs
+++ Business/Business.cs
@@ -2,7 +2,13 @@
 
 namespace Business
 {
+    public enum TipoConta { Poupanca, Corrente, Investimento };
+
     public class ContaBancaria
     {
+        public string banco { get; set; }
+        public int agencia { get; set; }
+        public int numero { get; set; }
+        public TipoConta tipo { get; set; }
     }
 }
